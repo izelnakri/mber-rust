2ecbf1a82c6202f4a68a2cfd30850e2a926b64a5
Izel Nakri <contact@izelnakri.com>
2019-09-08 18:15:17 +0200

    tests for basic print commands

---
+++ tests/main_test.rs
@@ -0,0 +1,57 @@
+use std::process::Command;
+use yansi::Paint;
+
+#[test]
+fn default_command_works() {
+    let command = Command::new("cargo")
+        .env("FORCE_COLOR", "0")
+        .arg("run")
+        .output()
+        .expect("Failed to execute the process");
+    let stdout = String::from_utf8(command.stdout).unwrap();
+
+    assert_eq!(command.status.success(), true);
+    assert_eq!(
+        stdout.contains("unknown command. Available options are:"),
+        false
+    );
+
+    [
+        "Usage: mber <command (Default: help)>",
+        "mber init | new                    # Sets up the initial ember folder structure",
+        "mber serve | server                # Starts your ember development server [alias: \"mber s\"]",
+        "mber build | b                     # Builds your ember application and outputs to /dist folder",
+        "mber console | c                   # Boots your ember application with DOM in a node.js repl",
+        "mber test | t                      # Runs your ember tests (--server to run them in browser)",
+        "mber generate | g [type] [name]    # Generate ember files for certain abstraction type",
+        "mber delete | d [type] [name]      # Remove ember files for certain abstraction type"
+    ].iter().for_each(|string| {
+        assert_eq!(stdout.contains(string), true);
+    })
+}
+
+#[test]
+fn test_invalid_command() {
+    let command = Command::new("cargo")
+        .env("FORCE_COLOR", "0")
+        .arg("run")
+        .arg("some_invalid_command")
+        .output()
+        .expect("Failed to execute the process");
+    let stdout = String::from_utf8(command.stdout).unwrap();
+
+    assert_eq!(command.status.success(), false);
+    [
+        "unknown command. Available options are:",
+        "Usage: mber <command (Default: help)>",
+        "mber init | new                    # Sets up the initial ember folder structure",
+        "mber serve | server                # Starts your ember development server [alias: \"mber s\"]",
+        "mber build | b                     # Builds your ember application and outputs to /dist folder",
+        "mber console | c                   # Boots your ember application with DOM in a node.js repl",
+        "mber test | t                      # Runs your ember tests (--server to run them in browser)",
+        "mber generate | g [type] [name]    # Generate ember files for certain abstraction type",
+        "mber delete | d [type] [name]      # Remove ember files for certain abstraction type"
+    ].iter().for_each(|string| {
+        assert_eq!(stdout.contains(string), true);
+    })
+}
