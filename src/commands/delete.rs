f7da362119e8cc340e850efce1bfa98ec1b2ff14
Izel Nakri <contact@izelnakri.com>
2019-10-08 16:15:19 +0200

    first working delete command

--- src/commands/delete.rs
+++ src/commands/delete.rs
@@ -1,5 +1,12 @@
-use super::super::utils::console;
+use std::env;
+use std::fs;
 use std::process;
+use std::io;
+use inflector::cases::snakecase::to_snake_case;
+use inflector::string::singularize::to_singular;
+use yansi::Paint;
+use super::super::utils;
+use super::super::utils::console;
 
 pub fn run() -> std::io::Result<()> {
     let abstraction = std::env::args()
@@ -18,15 +25,60 @@ pub fn run() -> std::io::Result<()> {
         process::exit(1);
     }
 
-    let _name = std::env::args().nth(3)
-        .unwrap_or_else(|| {
-            console::error(format!("mber d {} missing a name to delete!", abstraction));
+    let remaining_args = env::args().skip(3).collect::<Vec<_>>();
 
-            process::exit(1);
-        });
+    if remaining_args.len() == 0 {
+        console::error(format!("mber d {} missing a name to delete!", abstraction));
 
+        process::exit(1);
+    }
+
+    let name = remaining_args.join(" ");
+    let project_root = utils::find_project_root().to_string_lossy().to_string();
+
+    match abstraction.as_str() {
+        "component" => {
+            let folder_name = to_snake_case(&to_singular(&name)).replace("_", "-");
+            let target_folder = format!("{}/src/ui/components/{}", project_root, folder_name);
+
+            check_if_path_exists(&target_folder, &project_root);
+            remove_folder(target_folder, project_root)?;
+        }
+        // "helper" =k
+        // "initializer" =>
+        // "instance_initializer" =>
+        // "mixin" =>
+        // "model" =>
+        // "route" =>
+        // "service" =>
+        // "util" =>
+        _ => ()
+    }
 
     Ok(())
+}
 
-    // return commands::destroy::run(abstraction, name);
+fn check_if_path_exists(path: &String, project_root: &String) {
+    if fs::metadata(&path).is_err() {
+        let target_path = path.replace(format!("{}/", project_root).as_str(), "");
+
+        console::error(format!("{} does not exist!", target_path));
+        process::exit(1);
+    }
+}
+
+// fn remove_file_if_exists(path: String, project_root: String) -> io::Result<()> {
+//     if fs::metadata(&path).is_ok() {
+//         fs::remove_file(&path)?;
+//         console::log(format!("{} {}", Paint::red("deleted"), path.replace(format!("{}/", project_root).as_str(), "")));
+//     }
+
+//     return Ok(());
+// }
+
+fn remove_folder(path: String, project_root: String) -> io::Result<()> {
+    fs::remove_dir_all(&path)?;
+    console::log(format!("{} {}", Paint::red("deleted"), path.replace(format!("{}/", project_root).as_str(), "")));
+
+    return Ok(());
 }
