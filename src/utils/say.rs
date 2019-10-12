1df09feb7e7ce3783d2c8f213fb50a1d36a03f1d
Izel Nakri <contact@izelnakri.com>
2019-10-12 23:56:11 +0200

    say::speak(text) doest break process and beeps when festival isnt in PATH

--- src/utils/say.rs
+++ src/utils/say.rs
@@ -1,15 +1,27 @@
 use std::process::{Command, Stdio};
 
 pub fn speak(text: &str) {
-    let text = Command::new("echo")
-        .arg(text)
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("");
+    if tts_program_in_path("festival") {
+        let text = Command::new("echo")
+            .arg(text)
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("");
 
-    Command::new("festival")
-        .args(&["--tts"])
-        .stdin(text.stdout.unwrap())
-        .spawn()
-        .expect("");
+        Command::new("festival")
+            .args(&["--tts"])
+            .stdin(text.stdout.unwrap())
+            .spawn()
+            .expect("");
+    } else {
+        println!("\007");
+    }
 }
+
+fn tts_program_in_path(program: &str) -> bool {
+    match Command::new(program).stdout(Stdio::null()).spawn() {
+        Ok(_) => true,
+        Err(_) => false
+    }
+}
+
